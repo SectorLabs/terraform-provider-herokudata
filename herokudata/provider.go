0974c54e857a177b02ed69c532b704b8a61586d6
Radu Toma <radu.toma@sectorlabs.ro>
2020-08-19 09:44:29 +0300

    Added validation for provider API key and URL

--- herokudata/provider.go
+++ herokudata/provider.go
@@ -2,20 +2,23 @@ package herokudata
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
 			"api_key": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("HEROKUDATA_API_KEY", nil),
+				Type:         schema.TypeString,
+				Required:     true,
+				DefaultFunc:  schema.EnvDefaultFunc("HEROKUDATA_API_KEY", nil),
+				ValidateFunc: validation.StringIsNotEmpty,
 			},
 			"url": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("HEROKUDATA_API_URL", DefaultURL),
+				Type:         schema.TypeString,
+				Required:     true,
+				DefaultFunc:  schema.EnvDefaultFunc("HEROKUDATA_API_URL", DefaultURL),
+				ValidateFunc: validation.StringIsNotEmpty,
 			},
 			"poll_timeout_duration": {
 				Type:     schema.TypeString,
